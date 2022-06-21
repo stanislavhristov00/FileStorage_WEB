8ea386665ab4ee3e3b52ebc0dacbac4d01b82a72
Stanislav Hristov <stanislavhristov00@abv.bg>
2022-06-22 00:31:31 +0300

    New sql script

--- web_coursework.sql
+++ web_coursework.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2022 at 11:25 PM
+-- Generation Time: Jun 21, 2022 at 11:29 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -24,10 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `files`
+--
+
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL,
+  `hash` varchar(32) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `files`
+--
+
+INSERT INTO `files` (`id`, `hash`, `name`, `user`) VALUES
+(4, 'b7f82afa97d76fc732b9b0d45ca4946e', 'test_imotche.pdf', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shared`
+--
+
+CREATE TABLE `shared` (
+  `file_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `shared`
+--
+
+INSERT INTO `shared` (`file_id`, `user_id`, `time`) VALUES
+(4, 1, '2022-06-20 17:15:26');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `username` varchar(256) NOT NULL,
   `h_password` varchar(256) NOT NULL,
   `email` varchar(256) NOT NULL
@@ -37,19 +77,68 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`username`, `h_password`, `email`) VALUES
-('b', 'ee31e7ebbc48dcea7cce9591ab8664d7d7d6938c', 'b@abv.bg');
+INSERT INTO `users` (`id`, `username`, `h_password`, `email`) VALUES
+(1, 'Gosho123@abv.bg', 'ee31e7ebbc48dcea7cce9591ab8664d7d7d6938c', 'Gosho123@abv.bg'),
+(2, 'a', 'ee31e7ebbc48dcea7cce9591ab8664d7d7d6938c', 'abv@abv.bg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`);
+
+--
+-- Indexes for table `shared`
+--
+ALTER TABLE `shared`
+  ADD PRIMARY KEY (`file_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`username`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `files`
+--
+ALTER TABLE `files`
+  ADD CONSTRAINT `FOREIGN KEY` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `shared`
+--
+ALTER TABLE `shared`
+  ADD CONSTRAINT `shared_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `shared_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
